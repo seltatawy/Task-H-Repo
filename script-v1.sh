e304cab273669681475ee871793e245f1a7ab8a6
seltatawy <shaima_tatawy@hotmail.com>
2021-10-04 05:47:01 -0400

    update script

--- script-v1.sh
+++ script-v1.sh
@@ -3,6 +3,10 @@
 cd /home/seltatawy/git-clone/
 git clone git@github.com:seltatawy/Task-H-Repo.git
 cd Task-H-Repo/
+#to get all branches in from remote git hub
+for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
+   git branch --track ${branch#remotes/origin/} $branch
+done
 git branch -a > branches
 chmod +x branches
 git add .
