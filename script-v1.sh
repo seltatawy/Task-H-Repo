e0bbd8daa969428ebf0881e939aa26c287d01630
seltatawy <shaima_tatawy@hotmail.com>
2021-10-03 23:31:45 -0400

    adding file

---
+++ script-v1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#script step 7 in task
+cd /home/seltatawy/git-clone/
+git clone git@github.com:seltatawy/Task-H-Repo.git
+git branch > branches
+chmod +x branches
+cd /home/seltatawy/git-repo-v3/Task-H-Repo/
+git checkout Red
+#search for string Hello in logs"
+git log -b Red --grep "Hello"
+#append 2 lines to branches file
+echo “ hello again” >> branches
