f711123c5f055151afe3fc2266b51c9de2c8238c
seltatawy <shaima_tatawy@hotmail.com>
2021-10-04 02:12:26 -0400

    update script

--- script-v1.sh
+++ script-v1.sh
@@ -4,6 +4,9 @@ cd /home/seltatawy/git-clone/
 git clone git@github.com:seltatawy/Task-H-Repo.git
 git branch > branches
 chmod +x branches
+git add .
+git commit -am "adding branches file"
+git push origin main
 cd /home/seltatawy/git-repo-v3/Task-H-Repo/
 git checkout Red
 #search for string Hello in logs"
