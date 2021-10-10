15d9ca99393bbbe029a8577479fbf056bd89ba4b
seltatawy <shaima_tatawy@hotmail.com>
2021-10-10 09:36:06 -0400

    adding script

--- script-v1.sh
+++ script-v1.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-#script step 7 in task
+cript step 7 in task
 cd /home/seltatawy/git-clone/
 git clone git@github.com:seltatawy/Task-H-Repo.git
 
 cd Task-H-Repo/
 #to get all branches in from remote git hub
+rm -f branches*
 for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v main `; do
    git branch --track ${branch#remotes/origin/} $branch
 done
 #list all branches and save to branches file
-git branch -a > branches
-chmod +x branches
-git add .
+git branch -a > branches_`date +%Y-%m-%d_%H:%M:%S`
+chmod +x branches*
+git add --all
 git commit -am "adding branches file"
 git push origin main
 pwd
@@ -25,15 +25,16 @@ git checkout Black
 #then copy the version of file to Black
 git checkout main script-v1.sh
 git status
-git add .
+git add --all
 git commit -am "adding branches file"
-git push origin Black
+git push -f origin Black
 #append 2 lines
 echo "hello again" >> script-v1.sh
 echo "I need a cup of coffee" >> script-v1.sh
-git add .
+git add --all
 git commit -am "adding 2 lines"
 git push origin Black
+
 
 
 
